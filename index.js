072f286d192e74187739cfb9d6267648c6819165
Rafael Pacas @rapala61 <rapala61@gmail.com>
2015-07-10 16:43:34 -0400

    fixed error with variable not being defined

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use('/', main);
 // });
 
 var server = app.listen(3000, function () {
-  // var host = server.address().address;
-  // var port = server.address().port;
+  var host = server.address().address;
+  var port = server.address().port;
   //
   console.log('Example app listening at http://%s:%s', host, port);
 });
