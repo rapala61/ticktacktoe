079bf1fdf26a7d679b04eaa592bf2649d25b1ebe
Rafael Pacas @rapala61 <rapala61@gmail.com>
2015-07-10 16:32:12 -0400

    introduce express for tictactoe

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var main = require('./app/controllers/main');
+
+// Use static files
+app.use(express.static('public'));
+
+// Routes
+app.use('/', main);
+
+// Handle 404
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+var server = app.listen(3001, function () {
+  // var host = server.address().address;
+  // var port = server.address().port;
+  //
+  // console.log('Example app listening at http://%s:%s', host, port);
+});
