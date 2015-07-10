e202a1e29631874f9aaf783ebd667eb6a91931f5
Rafael Pacas @rapala61 <rapala61@gmail.com>
2015-07-10 16:38:23 -0400

    changed port to 3000

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
 });
 
-var server = app.listen(3001, function () {
+var server = app.listen(3000, function () {
   // var host = server.address().address;
   // var port = server.address().port;
   //
